df1b5a83931d949d76e9ec93640687705f3453b6
bassam hmod <bassamhmoud8@gmail.com>
2026-07-04 11:00:33 +0300

    Fix AppUpdate migration

--- multistore/settings.py
+++ multistore/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "change-me"
 
-DEBUG = True
+#DEBUG = True
 ADMINS = [("Admin", "admin@example.com")]
 LOGGING = {
     "version": 1,
