27b397ec4ef8babcefe7e09ee0af949b46cd9ce7
bassam hmod <bassamhmoud8@gmail.com>
2026-07-04 10:36:58 +0300

    Fix AppUpdate migration

---
+++ create_superuser.py
@@ -0,0 +1,23 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multistore.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+USERNAME = "admin"
+EMAIL = ""
+PASSWORD = "0966836013"
+
+if not User.objects.filter(username=USERNAME).exists():
+    User.objects.create_superuser(
+        username=USERNAME,
+        email=EMAIL,
+        password=PASSWORD,
+    )
+    print("✅ Superuser created.")
+else:
+    print("ℹ️ Superuser already exists.")
