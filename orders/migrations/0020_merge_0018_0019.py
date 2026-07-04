6dc680ad40228cb955fd2d597001e80578c8b33e
bassam hmod <bassamhmoud8@gmail.com>
2026-07-04 09:33:26 +0300

    Fix AppUpdate migration

--- orders/migrations/0020_merge_0018_0019.py
+++ orders/migrations/0020_merge_0018_0019.py
@@ -2,7 +2,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('orders', '0018_alter_order_created_by_store_user_and_more'),
+        ('orders', '0018_alter_order_created_by_store_user'),
         ('orders', '0019_order_mobile_sync_fields'),
     ]
 
