ffb41b635606395132428c554cb049e90348e37b
bassam hmod <bassamhmoud8@gmail.com>
2026-07-20 11:47:04 +0300

    Use non-manifest WhiteNoise storage

--- multistore/settings.py
+++ multistore/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = "/media/"
