eea9ab0aca61298315b1a984a377cf4e03444dad
bassam hmod <bassamhmoud8@gmail.com>
2026-07-20 11:08:06 +0300

    Relax WhiteNoise static manifest checks

--- multistore/settings.py
+++ multistore/settings.py
@@ -162,6 +162,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = Path(os.getenv("DJANGO_MEDIA_ROOT", BASE_DIR / "media"))
