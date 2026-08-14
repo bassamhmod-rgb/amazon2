5ee812304dfd9527403a38ed3a2960270edca95a
bassam hmod <bassamhmoud8@gmail.com>
2026-08-14 13:14:27 +0300

    Merge accounts migration branches

---
+++ accounts/migrations/0022_merge_0019_alter_appupdate_prices_version_and_more_0021_customer_is_subscription_active.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("accounts", "0019_alter_appupdate_prices_version_and_more"),
+        ("accounts", "0021_customer_is_subscription_active"),
+    ]
+
+    operations = []
